956c2346034bd35f30e407febda9d32104ec3a4a
Samuel Sutch <samuraiblog@gmail.com>
2013-09-19 20:58:10 -0700

    full size ticker implemented

--- FMF Football/FFTickerMaximizedDrawerViewController.m
+++ FMF Football/FFTickerMaximizedDrawerViewController.m
@@ -63,7 +63,12 @@
                   layout:(UICollectionViewLayout *)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    return CGSizeMake(174, 65);
+    NSDictionary *player = [self.tickerData objectAtIndex:indexPath.row];
+    
+    NSString *nameValue = [NSString stringWithFormat:@"%@ (%@)", player[@"name"], player[@"position"]];
+    CGFloat namw = [nameValue sizeWithFont:[FFStyle regularFont:15]
+                         constrainedToSize:CGSizeMake(150, 100)].width;
+    return CGSizeMake(56+namw, 65);
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
@@ -73,15 +78,50 @@
     [cell.contentView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
     
     NSDictionary *player = [self.tickerData objectAtIndex:indexPath.row];
-    NSLog(@"player: %@", player);
     
     UIImageView *img = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 56, 56)];
+    img.contentMode = UIViewContentModeScaleAspectFit;
     [cell.contentView addSubview:img];
+    [img setImageWithURL:[NSURL URLWithString:player[@"headshot_url"]] placeholderImage:[UIImage imageNamed:@"helmet-placeholder.png"]];
     
-    cell.backgroundColor = [UIColor purpleColor];
-    cell.backgroundView.backgroundColor = [UIColor purpleColor];
-    cell.backgroundView.layer.borderColor = [UIColor blackColor].CGColor;
-    cell.backgroundView.layer.borderWidth = 1;
+    UIImageView *overlay = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 56, 56)];
+    overlay.contentMode = UIViewContentModeScaleAspectFit;
+    overlay.image = [UIImage imageNamed:@"player-cutout.png"];
+    overlay.backgroundColor = [UIColor clearColor];
+    [cell.contentView addSubview:overlay];
+    
+    NSString *nameValue = [NSString stringWithFormat:@"%@ (%@)", player[@"name"], player[@"position"]];
+    CGFloat namw = [nameValue sizeWithFont:[FFStyle regularFont:15]
+                         constrainedToSize:CGSizeMake(150, 100)].width;
+    UILabel *nam = [[UILabel alloc] initWithFrame:CGRectMake(56, 5, namw, 15)];
+    nam.font = [FFStyle regularFont:15];
+    nam.textColor = [FFStyle white];
+    nam.backgroundColor = [FFStyle darkGreen];
+    nam.text = nameValue;
+    [cell.contentView addSubview:nam];
+    
+    NSString *ppgValue = [NSString stringWithFormat:@"%@ PPG", player[@"ppg"]];
+    UILabel *ppg = [[UILabel alloc] initWithFrame:CGRectMake(56, 23, 60, 15)];
+    ppg.textColor = [FFStyle white];
+    ppg.backgroundColor = [FFStyle darkGreen];
+    ppg.font = [FFStyle regularFont:12];
+    ppg.text = ppgValue;
+    [cell.contentView addSubview:ppg];
+    
+    double currency = [player[@"buy_price"] doubleValue];
+    static NSNumberFormatter *formatter = nil;
+    if (!formatter) {
+        formatter = [[NSNumberFormatter alloc] init];
+        [formatter setNumberStyle: NSNumberFormatterCurrencyStyle];
+    }
+    NSString *costValue = [formatter stringFromNumber:[NSNumber numberWithDouble:currency]];
+    UILabel *cost = [[UILabel alloc] initWithFrame:CGRectMake(56, 38, 100, 15)];
+    cost.textColor = [FFStyle white];
+    cost.backgroundColor = [FFStyle darkGreen];
+    cost.font = [FFStyle regularFont:12];
+    cost.text = costValue;
+    [cell.contentView addSubview:cost];
+    
     return cell;
 }
 
