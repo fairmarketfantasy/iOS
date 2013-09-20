82453fd1f4fdf0e95bb5076ee2d028e4be097849
Samuel Sutch <samuraiblog@gmail.com>
2013-09-19 21:03:41 -0700

    minimized ticker implemented

--- FMF Football/FFTickerMinimizedDrawerViewController.m
+++ FMF Football/FFTickerMinimizedDrawerViewController.m
@@ -14,11 +14,11 @@
 
 @implementation FFTickerMinimizedDrawerViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)init
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super init];
     if (self) {
-        // Custom initialization
+        [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"MinCell"];
     }
     return self;
 }
@@ -45,6 +45,50 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *player = [self.tickerData objectAtIndex:indexPath.row];
+    
+    NSString *nameValue = [NSString stringWithFormat:@"%@ (%@)", player[@"name"], player[@"position"]];
+    CGFloat namw = [nameValue sizeWithFont:[FFStyle regularFont:15]
+                         constrainedToSize:CGSizeMake(150, 100)].width;
+    return CGSizeMake(56+namw, 48);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MinCell" forIndexPath:indexPath];
+    
+    [cell.contentView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
+    NSDictionary *player = [self.tickerData objectAtIndex:indexPath.row];
+    
+    UIImageView *img = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 48, 48)];
+    img.contentMode = UIViewContentModeScaleAspectFit;
+    [cell.contentView addSubview:img];
+    [img setImageWithURL:[NSURL URLWithString:player[@"headshot_url"]] placeholderImage:[UIImage imageNamed:@"helmet-placeholder.png"]];
+    
+    UIImageView *overlay = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 48, 48)];
+    overlay.contentMode = UIViewContentModeScaleAspectFit;
+    overlay.image = [UIImage imageNamed:@"player-cutout.png"];
+    overlay.backgroundColor = [UIColor clearColor];
+    [cell.contentView addSubview:overlay];
+    
+    NSString *nameValue = [NSString stringWithFormat:@"%@ (%@)", player[@"name"], player[@"position"]];
+    CGFloat namw = [nameValue sizeWithFont:[FFStyle regularFont:14]
+                         constrainedToSize:CGSizeMake(150, 100)].width;
+    UILabel *nam = [[UILabel alloc] initWithFrame:CGRectMake(48, 15, namw, 15)];
+    nam.font = [FFStyle regularFont:14];
+    nam.textColor = [FFStyle white];
+    nam.backgroundColor = [FFStyle darkGreen];
+    nam.text = nameValue;
+    [cell.contentView addSubview:nam];
+    
+    return cell;
 }
 
 @end
