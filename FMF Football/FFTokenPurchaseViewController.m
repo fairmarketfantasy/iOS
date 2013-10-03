3505d0a39087ea56d30e0219ea85c36e3e379980
Samuel Sutch <samuraiblog@gmail.com>
2013-10-03 17:18:57 -0600

    more work on purchasing tokens

--- FMF Football/FFTokenPurchaseViewController.m
+++ FMF Football/FFTokenPurchaseViewController.m
@@ -9,14 +9,19 @@
 #import "FFTokenPurchaseViewController.h"
 #import "FFSessionViewController.h"
 #import "FFAlertView.h"
-#import <IAPManager/IAPManager.h>
+//#import <IAPManager/IAPManager.h>
 #import <StoreKit/StoreKit.h>
+#import "NSData+Base64.h"
 
 
-@interface FFTokenPurchaseViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface FFTokenPurchaseViewController ()
+<UITableViewDataSource, UITableViewDelegate, SKPaymentTransactionObserver, SKProductsRequestDelegate>
 
 @property (nonatomic) UITableView *tableView;
 @property (nonatomic) NSArray *purchaseItems;
+@property (nonatomic) FFAlertView *alert;
+@property (nonatomic) NSArray *skus;
+@property (nonatomic) BOOL restoring;
 
 @end
 
@@ -68,32 +73,53 @@
 {
     [super viewDidAppear:animated];
     
-    FFAlertView *alert = [[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Loading", nil)
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    _restoring = YES;
+    
+    if (![SKPaymentQueue canMakePayments]) {
+        FFAlertView *alert =[[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Error", nil)
+                                                        message:NSLocalizedString(@"Purchasing is not enabled for this Apple account", nil)
+                                              cancelButtonTitle:nil okayButtonTitle:NSLocalizedString(@"Dismiss", nil) autoHide:YES];
+        [alert showInView:self.view];
+        return;
+    }
+    
+    _alert = [[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Loading", nil)
                                                    messsage:nil
                                                loadingStyle:FFAlertViewLoadingStylePlain];
-    [alert showInView:self.view];
+    [_alert showInView:self.view];
     
     [self.session authorizedJSONRequestWithMethod:@"GET" path:@"/users/token_plans" paramters:@{} success:
      ^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, id JSON) {
          
-         NSArray *items = [JSON allKeys];
-         [[IAPManager sharedIAPManager] getProductsForIds:items completion:^(NSArray *products) {
-             self.purchaseItems = products;
-             [self.tableView reloadData];
-             [alert hide];
-         }];
-         
-         [alert hide];
-         
-         [self.tableView reloadData];
+         _skus = [JSON allKeys];
+         [self getProducts];
+
      } failure:^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, NSError *error, id JSON) {
-         [alert hide];
+         [_alert hide];
          FFAlertView *ealert = [[FFAlertView alloc] initWithError:error title:nil
                                                 cancelButtonTitle:nil
                                                   okayButtonTitle:NSLocalizedString(@"Dismiss", nil)
                                                          autoHide:YES];
          [ealert showInView:self.view];
      }];
+}
+
+- (void)getProducts
+{
+    SKProductsRequest *req = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithArray:_skus]];
+    req.delegate = self;
+    [req start];
+}
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+    _purchaseItems = response.products;
+    [_tableView reloadData];
+    if (_alert) {
+        [_alert hide];
+        _alert = nil;
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -146,7 +172,13 @@
     SKProduct *prod = self.purchaseItems[indexPath.row];
     cell.textLabel.text = prod.localizedTitle;
     
-    FFCustomButton *_select = [FFStyle coloredButtonWithText:NSLocalizedString(@"Buy", nil)
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    [numberFormatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
+    [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    [numberFormatter setLocale:prod .priceLocale];
+    NSString *formattedString = [numberFormatter stringFromNumber:prod.price];
+    
+    FFCustomButton *_select = [FFStyle coloredButtonWithText:formattedString
                                                        color:[FFStyle brightGreen]
                                                  borderColor:[FFStyle white]];
     _select.tag = indexPath.row; // the tag is what index is selected
@@ -159,45 +191,112 @@
 
 - (void)select:(UIButton *)sender
 {
-    FFAlertView *alert = [[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Purchasing", nil)
-                                                   messsage:nil loadingStyle:FFAlertViewLoadingStylePlain];
-    [alert showInView:self.view];
+    _alert = [[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Purchasing", nil)
+                                       messsage:nil
+                                   loadingStyle:FFAlertViewLoadingStylePlain];
+    [_alert showInView:self.view];
     
     SKProduct *product = self.purchaseItems[sender.tag];
+    SKPayment *payment = [SKPayment paymentWithProduct:product];
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
     
-    [[IAPManager sharedIAPManager] purchaseProduct:product completion:^(SKPaymentTransaction *transaction) {
-        NSDictionary *params = @{@"receipt": [NSString stringWithUTF8String:transaction.transactionReceipt.bytes]};
-        
-        [self.session authorizedJSONRequestWithMethod:@"POST" path:@"/users/add_tokens" paramters:params success:
-         ^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, id JSON) {
-             [alert hide];
-             FFAlertView *salert = [[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Success", nil)
-                                                              message:NSLocalizedString(@"Your purchase was successful. "
-                                                                                        @"Your balance will be updated shortly.", nil)];
-             [salert showInView:self.view];
-             double delayInSeconds = 1.5;
-             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                 [salert hide];
-             });
-         } failure:^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, NSError *error, id JSON) {
-             [alert hide];
-             FFAlertView *ealert = [[FFAlertView alloc] initWithError:error
-                                                                title:nil
-                                                    cancelButtonTitle:nil
-                                                      okayButtonTitle:NSLocalizedString(@"Dissmiss", nil)
-                                                             autoHide:YES];
-             [ealert showInView:self.view];
-         }];
-    } error:^(NSError *error) {
-        [alert hide];
-        FFAlertView *ealert = [[FFAlertView alloc] initWithError:error
-                                                           title:nil
-                                               cancelButtonTitle:nil
-                                                 okayButtonTitle:NSLocalizedString(@"Dissmiss", nil)
-                                                        autoHide:YES];
-        [ealert showInView:self.view];
-    }];
+//    [[IAPManager sharedIAPManager] purchaseProduct:product completion:^(SKPaymentTransaction *transaction) {
+//        NSDictionary *params = @{@"receipt": [transaction.transactionReceipt base64EncodedString]};
+//        
+//        [self.session authorizedJSONRequestWithMethod:@"POST" path:@"/users/add_tokens" paramters:params success:
+//         ^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, id JSON) {
+//             [alert hide];
+//             FFAlertView *salert = [[FFAlertView alloc] initWithTitle:NSLocalizedString(@"Success", nil)
+//                                                              message:NSLocalizedString(@"Your purchase was successful. "
+//                                                                                        @"Your balance will be updated shortly.", nil)];
+//             [salert showInView:self.view];
+//             double delayInSeconds = 1.5;
+//             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+//             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+//                 [salert hide];
+//             });
+//         } failure:^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, NSError *error, id JSON) {
+//             [alert hide];
+//             FFAlertView *ealert = [[FFAlertView alloc] initWithError:error
+//                                                                title:nil
+//                                                    cancelButtonTitle:nil
+//                                                      okayButtonTitle:NSLocalizedString(@"Dissmiss", nil)
+//                                                             autoHide:YES];
+//             [ealert showInView:self.view];
+//         }];
+//    } error:^(NSError *error) {
+//        [alert hide];
+//        FFAlertView *ealert = [[FFAlertView alloc] initWithError:error
+//                                                           title:nil
+//                                               cancelButtonTitle:nil
+//                                                 okayButtonTitle:NSLocalizedString(@"Dissmiss", nil)
+//                                                        autoHide:YES];
+//        [ealert showInView:self.view];
+//    }];
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue removedTransactions:(NSArray *)transactions
+{
+    NSLog(@"removed transactions %@", transactions);
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue restoreCompletedTransactionsFailedWithError:(NSError *)error
+{
+    NSLog(@"restore completed transactions failed %@", error);
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedDownloads:(NSArray *)downloads
+{
+    NSLog(@"update downloads %@", downloads);
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions
+{
+    NSLog(@"updated transactions %@", transactions);
+
+    for (SKPaymentTransaction *t in transactions) {
+        switch (t.transactionState) {
+            case SKPaymentTransactionStatePurchased:
+                NSLog(@"transaction purchased %@ %@", t, t.payment);
+                [self addTokens:t];
+                break;
+            case SKPaymentTransactionStateRestored:
+                NSLog(@"transaction restored %@ %@", t, t.payment);
+                break;
+            case SKPaymentTransactionStatePurchasing:
+                NSLog(@"transaction purchasing %@ %@", t, t.payment);
+                break;
+            case SKPaymentTransactionStateFailed:
+                NSLog(@"transaction failed %@ %@ %@", t, t.payment, t.error);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+- (void)addTokens:(SKPaymentTransaction *)transaction
+{
+    NSLog(@"add tokens %@", transaction);
+    
+    NSDictionary *params = @{@"receipt": [transaction.transactionReceipt base64EncodedString]};
+    NSLog(@"params %@", params);
+    
+    [self.session authorizedJSONRequestWithMethod:@"POST" path:@"/users/add_tokens" paramters:params success:
+     ^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, id JSON) {
+         
+         [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+         
+         [[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+         
+     } failure:^(NSURLRequest *request, NSHTTPURLResponse *httpResponse, NSError *error, id JSON) {
+         NSLog(@"error verifying transaction %@", error);
+     }];
+}
+
+- (void)paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
+{
+    NSLog(@"restore completed transactions finished");
 }
 
 @end
