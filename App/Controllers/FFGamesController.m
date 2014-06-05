f1160a5f73f136659f56b8566863b0f2bf68e904
Anton Chuev <chuev_anton@inbox.ru>
2014-06-05 16:27:59 +0300

    add receiver controllers for fantasy and non fantasy

---
+++ App/Controllers/FFGamesController.m
@@ -0,0 +1,401 @@
+//
+//  FFGamesController.m
+//  FMF Football
+//
+//  Created by Anton Chuev on 6/5/14.
+//  Copyright (c) 2014 FairMarketFantasy. All rights reserved.
+//
+
+#import "FFGamesController.h"
+#import "FFWideReceiverController.h"
+#import "FFYourTeamDataSource.h"
+#import "FFPagerController.h"
+#import "FFWideReceiverTable.h"
+#import "FFWideReceiverCell.h"
+#import "FFTeamAddCell.h"
+#import "FFNoConnectionCell.h"
+#import "FFCollectionMarketCell.h"
+#import "FFNonFantasyGameCell.h"
+#import "FFMarketsCell.h"
+#import "FFAutoFillCell.h"
+#import "FFRosterTableHeader.h"
+#import "FFMarketSelector.h"
+#import "FFAccountHeader.h"
+#import "FFStyle.h"
+#import "FFMarket.h"
+#import "FFDate.h"
+#import "FFMarketSet.h"
+#import "FFPathImageView.h"
+#import "FFPTController.h"
+#import "FFAlertView.h"
+#import "Reachability.h"
+#import "UIImageView+UIActivityIndicatorForSDWebImage.h"
+#import <libextobjc/EXTScope.h>
+#import <libextobjc/EXTScope.h>
+#import <FlatUIKit.h>
+// model
+#import "FFUser.h"
+#import "FFRoster.h"
+#import "FFPlayer.h"
+#import "FFTeam.h"
+#import "FFNonFantasyGame.h"
+#import "FFSessionManager.h"
+
+@interface FFGamesController () <UITableViewDataSource, UITableViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate, FFMarketSelectorDelegate, FFMarketSelectorDataSource>
+
+@property(nonatomic, assign) NSUInteger position;
+@property(nonatomic, assign) NetworkStatus networkStatus;
+@property(nonatomic, assign) BOOL isServerError;
+
+@property(nonatomic) FFMarketSet* marketsSetRegular;
+@property(nonatomic) FFMarketSet* marketsSetSingle;
+@property(nonatomic, assign) NSUInteger tryCreateRosterTimes;
+
+@end
+
+@implementation FFGamesController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.position = 0;
+    self.tableView = [[FFWideReceiverTable alloc] initWithFrame:self.view.bounds];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    
+    //refresh control
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    refreshControl.tintColor = [FFStyle lightGrey];
+    [refreshControl addTarget:self action:@selector(pullToRefresh:) forControlEvents:UIControlEventValueChanged];
+    [self.tableView addSubview:refreshControl];
+    
+    //reachability
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(checkNetworkStatus:) name:kReachabilityChangedNotification object:nil];
+    internetReachability = [Reachability reachabilityForInternetConnection];
+	BOOL success = [internetReachability startNotifier];
+	if ( !success )
+		DLog(@"Failed to start notifier");
+    self.networkStatus = [internetReachability currentReachabilityStatus];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kReachabilityChangedNotification object:nil];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    if ([[FFSessionManager shared].currentCategoryName isEqualToString:FANTASY_SPORTS]) {
+        if (self.players.count == 0) {
+            [self fetchPlayersWithShowingAlert:NO completion:^{
+                [self.tableView reloadData];
+            }];
+        } else {
+            [self.tableView reloadData];
+        }
+    } else {
+        [self.tableView reloadData];
+    }
+}
+
+#pragma mark
+
+- (void)pullToRefresh:(UIRefreshControl *)refreshControl
+{
+    if ([[FFSessionManager shared].currentCategoryName isEqualToString:FANTASY_SPORTS]) {
+        [self fetchPlayersWithShowingAlert:NO completion:^{
+            [self.tableView reloadData];
+            [refreshControl endRefreshing];
+        }];
+    } else {
+        [self.delegate fetchGamesShowAlert:NO withCompletion:^{
+            [self.tableView reloadData];
+            [refreshControl endRefreshing];
+        }];
+    }
+}
+
+#pragma mark -
+
+- (void)checkNetworkStatus:(NSNotification *)notification
+{
+    NetworkStatus internetStatus = [internetReachability currentReachabilityStatus];
+    NetworkStatus previousStatus = self.networkStatus;
+    
+    if (internetStatus != self.networkStatus) {
+        self.networkStatus = internetStatus;
+        
+        if ((internetStatus != NotReachable && previousStatus == NotReachable) ||
+            (internetStatus == NotReachable && previousStatus != NotReachable)) {
+            
+            if (internetStatus == NotReachable) {
+                [self.tableView reloadData];
+            }
+        }
+    }
+}
+
+#pragma mark - public
+
+- (void)resetPosition
+{
+    self.position = 0;
+}
+
+- (void)reloadWithServerError:(BOOL)isError
+{
+    self.isServerError = isError;
+    [self.tableView reloadData];
+}
+
+- (void)showPosition:(NSString*)position
+{
+    if ([self.dataSource uniquePositions].count == 0) {
+        return;
+    }
+    
+    for (NSString* positionName in [self.dataSource uniquePositions]) {
+        if ([positionName isEqualToString:position]) {
+            [self selectPosition:[[self.dataSource uniquePositions] indexOfObject:positionName]];
+            NSUInteger selectedIndex = [[self.dataSource uniquePositions] indexOfObject:positionName];
+            break;
+        }
+    }
+}
+
+#pragma mark - private
+
+- (BOOL)isSomethingWrong
+{
+    return (self.networkStatus == NotReachable ||
+            self.isServerError ||
+            self.dataSource.unpaidSubscription == YES ||
+            [self.dataSource availableGames].count == 0);
+}
+
+- (void)selectPosition:(NSUInteger)position
+{
+    self.position = position;
+    [self fetchPlayersWithShowingAlert:YES completion:^{
+        [self.tableView reloadData];
+    }];
+}
+
+- (void)fetchPlayersWithShowingAlert:(BOOL)shouldShow completion:(void(^)(void))block
+{
+    if ([self isSomethingWrong]) {
+        block();
+        return;
+    }
+    
+    if (![self.dataSource.currentRoster objId]) {
+        self.players = @[];
+        [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:1]
+                      withRowAnimation:UITableViewRowAnimationAutomatic];
+        return;
+    }
+    
+    __block FFAlertView* alert = nil;
+    if (shouldShow) {
+        alert = [[FFAlertView alloc] initWithTitle:@"Loading Players"
+                                          messsage:nil
+                                      loadingStyle:FFAlertViewLoadingStylePlain];
+        [alert showInView:self.navigationController.view];
+    }
+    
+    __block BOOL shouldRemoveBenched = [[self.dataSource currentRoster] removeBenched].integerValue == 1;
+    
+    @weakify(self)
+    [FFPlayer fetchPlayersForRoster:[self.dataSource rosterId]
+                           position:[self.dataSource uniquePositions][self.position]
+                     removedBenched:shouldRemoveBenched
+                            session:self.session
+                            success:
+     ^(id successObj) {
+         @strongify(self)
+         self.isServerError = NO;
+         self.players = successObj;
+         //         [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:1]
+         //                       withRowAnimation:UITableViewRowAnimationAutomatic];
+         if(alert)
+             [alert hide];
+         if (block)
+             block();
+     }
+                            failure:
+     ^(NSError *error) {
+         @strongify(self)
+         self.players = @[];
+         //         [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:1]
+         //                       withRowAnimation:UITableViewRowAnimationAutomatic];
+         if(alert)
+             [alert hide];
+         if (block)
+             block();
+     }];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView*)tableView
+{
+    return [self isSomethingWrong] ? 1 : 2;
+}
+
+- (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) {
+        if ([self isSomethingWrong]) {
+            return 1;
+        }
+        
+        return 1;
+    }
+    
+    return [self.dataSource availableGames].count;
+}
+
+- (CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath
+{
+    if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+            //navigation bar height + status bar height = 64
+            return [self isSomethingWrong] ? [UIScreen mainScreen].bounds.size.height - 64.f : 70.f;
+        } else {
+            return 76.f;
+        }
+    }
+    
+    return 100.f;
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
+{
+    if (indexPath.section == 0) {
+        if ([self isSomethingWrong]) {
+            FFNoConnectionCell* cell = [tableView dequeueReusableCellWithIdentifier:kNoConnectionCellIdentifier
+                                                                       forIndexPath:indexPath];
+            NSString *message = nil;
+            if (self.networkStatus == NotReachable) {
+                message = @"No Internet Connection";
+            } else if ([self.dataSource unpaidSubscription]) {
+                message = @"Your free trial has ended. We hope you have enjoyed playing. To continue please visit our site: https//:predictthat.com";
+            } else if ([self.dataSource availableGames].count == 0) {
+                message = @"No Games Scheduled";
+            }
+            
+            cell.message.text = message;
+            return cell;
+        }
+        
+        return [self provideAutoFillCellForTable:tableView atIndexPath:indexPath];
+
+    } else if (indexPath.section == 1) {
+        FFNonFantasyGame *game = [[self.dataSource availableGames] objectAtIndex:indexPath.row];
+        return [self provideGameCellForGame:game table:tableView atIndexPath:indexPath];
+    }
+    
+    return nil;
+}
+
+#pragma mark - Cells
+
+- (FFNonFantasyGameCell *)provideGameCellForGame:(FFNonFantasyGame *)game table:(UITableView *)tableView atIndexPath:(NSIndexPath *)indexPath
+{
+    FFNonFantasyGameCell *cell = [tableView dequeueReusableCellWithIdentifier:kNonFantasyGameCellIdentifier
+                                                                 forIndexPath:indexPath];
+    [cell setupWithGame:game];
+    
+    __weak FFGamesController *weakSelf = self;
+    [cell.addHomeTeamBtn setAction:kUIButtonBlockTouchUpInside
+                         withBlock:^{
+                             FFNonFantasyGame *game = [weakSelf.dataSource availableGames][indexPath.row];
+                             [weakSelf.delegate addTeam:[game homeTeam]];
+                         }];
+    
+    [cell.addAwayTeamBtn setAction:kUIButtonBlockTouchUpInside
+                         withBlock:^{
+                             FFNonFantasyGame *game = [weakSelf.dataSource availableGames][indexPath.row];
+                             [weakSelf.delegate addTeam:[game awayTeam]];
+                         }];
+    
+    [cell.homePTButton setAction:kUIButtonBlockTouchUpInside
+                       withBlock:^{
+                           [self.delegate makeIndividualPredictionOnTeam:game.homeTeam];
+                       }];
+    
+    [cell.awayPTButton setAction:kUIButtonBlockTouchUpInside
+                       withBlock:^{
+                           [self.delegate makeIndividualPredictionOnTeam:game.awayTeam];
+                       }];
+    
+    return cell;
+}
+
+- (FFAutoFillCell *)provideAutoFillCellForTable:(UITableView *)tableView atIndexPath:(NSIndexPath *)indexPath
+{
+    FFAutoFillCell* cell = [tableView dequeueReusableCellWithIdentifier:kAutoFillCellIdentifier
+                                                           forIndexPath:indexPath];
+    [cell.autoFillButton addTarget:self
+                            action:@selector(autoFill:)
+                  forControlEvents:UIControlEventTouchUpInside];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
+{
+    // TODO: implement
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    if (section == 1) {
+        FFRosterTableHeader* view = [FFRosterTableHeader new];
+        view.titleLabel.text = @"Games for today";
+        return view;
+
+        NSString* positionName = @"";
+        if (self.delegate && self.networkStatus != NotReachable && [self.dataSource uniquePositions].count > 0) {
+            positionName = [self.dataSource uniquePositions][self.position];
+            NSString* positionFullName = self.dataSource.positionsNames[positionName];
+            if (positionFullName) {
+                positionName = positionFullName;
+            }
+        }
+        view.titleLabel.text = positionName;
+        view.priceLabel.text = self.delegate ?
+        [[FFStyle priceFormatter] stringFromNumber:@([[self.dataSource currentRoster] remainingSalary].floatValue)] : @"";
+        view.priceLabel.textColor = [[self.dataSource currentRoster] remainingSalary].floatValue > 0.f
+        ? [FFStyle brightGreen] : [FFStyle brightRed];
+        return view;
+    }
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 1) {
+        return 40.f;
+    }
+    return 0.f;
+}
+
+#pragma mark - button actions
+
+- (void)segments:(FUISegmentedControl*)segments
+{
+    [self selectPosition:segments.selectedSegmentIndex];
+}
+
+- (void)autoFill:(id)sender
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"Autofill" object:nil];
+}
+
+@end
---
+++ App/Controllers/FFPlayersController.h
@@ -0,0 +1,42 @@
+//
+//  FFPlayersController.h
+//  FMF Football
+//
+//  Created by Anton Chuev on 6/5/14.
+//  Copyright (c) 2014 FairMarketFantasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FFBaseViewController.h"
+#import "FFYourTeamDataSource.h"
+
+@class FFPlayer;
+@class FFTeam;
+
+@protocol FFPlayersProtocol <NSObject>
+
+- (void)addPlayer:(FFPlayer*)player;
+- (void)addTeam:(FFTeam *)team;
+- (void)fetchGamesShowAlert:(BOOL)shouldShow withCompletion:(void(^)(void))block;
+- (void)makeIndividualPredictionOnTeam:(FFTeam *)team;
+
+@end
+
+@class Reachability;
+
+@interface FFPlayersController : FFBaseViewController {
+    Reachability* internetReachability;
+}
+
+@property(nonatomic) NSArray* players; // should contain FFPlayer*
+@property(nonatomic, weak) id<FFPlayersProtocol> delegate;
+@property(nonatomic, weak) id<FFYourTeamDataSource> dataSource;
+@property(nonatomic) UITableView* tableView;
+
+- (void)showPosition:(NSString*)position;
+- (void)resetPosition;
+- (void)reloadWithServerError:(BOOL)isError;
+- (void)fetchPlayersWithShowingAlert:(BOOL)shouldShow completion:(void(^)(void))block;
+
+@end
+
